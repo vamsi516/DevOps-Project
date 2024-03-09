bf712909675fc89ad3a2109f217d2dc9260756fe
vamsi516 <66365971+vamsi516@users.noreply.github.com>
2024-03-09 14:43:06 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "amazon-linux" {
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
+  key_name = "project_key"
 
   tags = {
     Environment = "dev"
